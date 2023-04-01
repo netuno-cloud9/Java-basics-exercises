196b6a30a7adadfb7d5604e2eba4e053c5b0439a
Code.Odissey <110955443+netuno-cloud9@users.noreply.github.com>
2023-04-01 00:02:27 -0300

    Create DespesaMes.java

---
+++ DespesaMes.java
@@ -0,0 +1,15 @@
+public class DespesaMes{
+    private int mes;
+    private float valor;
+
+    public DespesaMes(int mes, float valor) {
+        this.mes = mes;
+        this.valor = valor;
+    }
+    public int getMes() {
+        return mes;
+    }
+    public float getValor() {
+        return valor;
+    }
+}
